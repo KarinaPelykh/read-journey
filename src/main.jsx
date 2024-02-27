fd1358d378813e684050ca2ff474ad67fa8e3864
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-02-27 16:37:10 +0100

    ADD: project settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import GlobalCss from "./stylesheet/globalStyles.js";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <GlobalCss />
+  </React.StrictMode>
+);
---
+++ src/stylesheet/theme.js
@@ -0,0 +1,35 @@
+export const theme = {
+  colors: {
+    white: "#F9F9F9",
+    grey: "#686868",
+    liteWhite: "#E3E3E3",
+    darkGray: "#262626",
+    lightBlack: "#1F1F1F",
+    black: "#141414",
+    blue: "#4F92F7",
+    lightGreen: "#30B94D",
+  },
+  fontSizes: {
+    xxxl: "64px",
+    xl: "20px",
+    l: "16px",
+    m: "18px",
+    s: "14px",
+    xs: "10px",
+  },
+  fontWeights: {
+    medium: 500,
+    regular: 700,
+  },
+  borders: {
+    none: "none",
+    normal: "1px solid",
+  },
+  radii: {
+    round: "100%",
+    xl: "  30px",
+    l: "15px",
+    m: "12px",
+    s: "8px",
+  },
+};
